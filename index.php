d597b60796ae0c7e99adcd946eae601da0278d12
agtomi <agtomi@gmail.com>
2018-07-10 21:41:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fitness</title>
+<link href="css/layout.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+<script src="js/main.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38135933-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<header>
+		<h2>Eleven Ház Jászárokszállás</h2>
+</header>
+
+<div class="container">
+		<div class="labels"> 
+				<a id="label1" class="label" href="#">Wellness részleg
+					<p>Az I. ütemben elkészülnek a szaunák: finn szauna, infra szauna és a gőzkabin. A II. ütemben a szaunákhoz kapcsolódni fog egy kisméretű medence.</p>
+					<span /> 
+				</a> 
+				<a id="label2" class="label" href="#">Recepció, Bár
+					<p>A recepción való bejelentkezés után tudod igénybe venni a ház szolgáltatásait.
+Találkozási pont, barátokkal, barátnőkkel beülhettek egy frissen őrölt kávéra, egy frissítő sencha teára, egy cappuccinóra extra habbal, forró csokira, friss narancslére. Megkívánsz egy mozzarellás pestos ciabattát, jó helyen jársz.</p>
+					<span /> 
+				</a> <a id="label3" class="label" href="#">Öltözők
+					<p>Kulturált Férfi és Női öltöző várja a fitness club látogatóit.</p>
+					<span /> 
+				</a> 
+				<a id="label4" class="label" href="#">Gyermekfoglalkoztató
+					<p>Amíg a szülők mozognak addig a gyerekek felnőtt felügyelete mellett játszhatnak a gyermekkuckóban.</p>
+					<span /> 
+				</a> 
+				<a id="label5" class="label" href="#">Fitness terem
+					<p>9 db kardió gép, közel 15 kondi gép és szabad súlyok várják a kezdő és haladó sportembereket.</p>
+					<span /> 
+				</a> 
+				<a id="label6" class="label" href="#">Nagyterem
+					<p>A csoportos foglalkozások tárháza: aerobic, kangoo, zumba, alakformáló órák, TRX, gerinctréning, pilates, jóga, nyugdíjas torna, gyerektorna, gyerek aerobic és a már nagyon várt spinning.</p>
+					<span /> 
+				</a> 
+				<a id="label7" class="label" href="#">Fallabda
+					<p>Az intenzív sportokat kedvelők számára a fallabda pálya nyújt sportolási és kikapcsolódási lehetőséget.</p>
+					<span /> 
+				</a> 
+                <a id="label8" class="label" href="#">Apartman ház
+					<p>A II. ütemben elkészülő apartman ház, 7 szobával várja majd a Jászárokszállásra érkező vendégeket.</p>
+					<span /> 
+				</a> 
+                <a id="label9" class="label" href="#">Masszázs
+					<p>Ha szeretne felfrissülni, itt jó helyen jár.</p>
+					<span /> 
+				</a> 
+                <a id="label10" class="label" href="#">Játszótér
+					<p>A kisgyermekek biztonságos körülmények között játszhatnak a tavasz beköszöntétől az ősz végéig, míg az Anyukák elfogyaszthatnak egy kávét vagy frissítő gyümölcslét, miközben megosztják egymással a nap híreit.</p>
+					<span /> 
+				</a> 
+                <a id="label11" class="label" href="#">Parkoló
+					<p>Ha autóval érkezel, jó körülmények között tudsz parkolni a 22 férőhelyes parkolóban.</p>
+					<span /> 
+				</a> 
+                <a id="label12" class="label" href="#">Teniszpálya
+					<p>A tenisz szerelmeseinek.</p>
+					<span /> 
+				</a> 
+                <a id="label13" class="label" href="#">Szolárium
+					<p>A barnulni vágyók számára.</p>
+					<span /> 
+				</a>
+		</div>
+			<div class="dialog">
+					<p> </p>
+				<a class="close">X</a> 
+			</div>
+    
+</div>
+<!--
+    <div class="nevado">
+          
+           <h4>Légy Te, aki nevet adsz az idei év nyarán megnyíló jászárokszállási Fitness létesítmény és Apartman háznak! 
+        </h4>    
+        <p>
+            Találjuk meg együtt a helynek a nevét ahová jó lesz betérni, összejönni és öröm lesz mozogni.</p>
+        <p>
+            Legyen ez a név magyaros hangzású, könnyen megjegyezhető, karakteres.</p>
+        <p>
+            Mindannyian szeretitek a zenét, a mozgás szabadságát, a baráti találkozásokat, a labda pattogását, a győzelem mámorát, egy jó beszélgetés ízét, a kávé illatát, a játék örömét, az ellazulást, a mozgás utáni pihenést.</p>
+        <p id="gondolkodj">
+        Gondolkodjatok rajta, hisz február 20-ig küldhetitek el <a href="https://www.facebook.com/photo.php?fbid=331624106939025&set=a.207683839333053.34219.207390212695749&type=1&theater" target="_blank">hozzászólásként</a> vagy a <a href="mailto:sportosjaszok@gmail.com">sportosjaszok@gmail.com</a> e-mail címre az ötleteiteket. </p>
+        <p>
+            Ha pár nap múlva újabb ötleted támad, bátran írd meg nekünk, mi örömmel fogadjuk. 
+        </p>
+       
+         
+        
+        
+    </div>
+-->
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(window).load(function() {
+    
+    // initialize of labels
+    $('.labels a#label1').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+        $('.labels a#label2').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+            $('.labels a#label3').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                $('.labels a#label4').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                    $('.labels a#label5').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                        $('.labels a#label6').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                            $('.labels a#label11').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                                $('.labels a#label8').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                                    $('.labels a#label12').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                                        $('.labels a#label10').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                                            $('.labels a#label9').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+                                                $('.labels a#label13').fadeIn(100).effect('bounce', { times:3 }, 300, function() {
+							$('.labels a#label7').fadeIn(100).effect('bounce', { times:3 }, 300);
+                    }); }); }); }); }); }); }); }); }); }); }); 
+	});
+    // dialog close
+    $('.dialog .close').click(function() {
+        $(this).parent().fadeOut(500);
+        return false;
+    });
+
+    // display dialog on click by labels
+    $('.labels a').click(function() {
+        $('.dialog p').html( $(this).find('p').html() ).parent().fadeIn(500);
+        return false;
+    });
+
+    // close dialog on click outside
+    $('.container').click(function() {
+        $('.dialog').fadeOut(500);
+    });
+});
